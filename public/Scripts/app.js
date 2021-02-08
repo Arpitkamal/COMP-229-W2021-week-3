bf5a06b0e314b8b8fa3a6271755ad7fbedd4d604
Arpit <kamalarpit@yahoo.in>
2021-02-08 11:55:51 +0530

    week 3 completed

---
+++ public/Scripts/app.js
@@ -0,0 +1,9 @@
+// IIFE -- Immediately Invoked Function Experssion
+(function(){
+
+    function Start()
+    {
+        console.log("App started...");
+    }
+    window.addEventListener("load", Start);
+})();
